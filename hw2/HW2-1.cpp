f108b64c840211679372857363362f8d799ed08f
Ziv <zhongturtle@gmail.com>
2018-11-17 23:58:39 +0800

    upload the basic setting

--- hw2/HW2-1.cpp
+++ hw2/HW2-1.cpp
@@ -1,2 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int n;
+int input_map[1000][1000];
+int value_map[1000][1000];
+
+int main() {
+	cin >> n;
+	for (int i = 0; i < n; i ++){
+		for (int j = 0; j < n; j ++){
+			cin >> input_map[i][j];
+    }
+  }
+
+	
+
+	cout << R << '\n';
+}
+
