9846421fc4934f80f60289bfa6c0da3275554059
Ziv <zhongturtle@gmail.com>
2018-11-18 23:32:48 +0800

    add calculate_value
    add a function called "calculate_value" which used to calculate the value of the difference between two blocks

--- hw2/HW2-1.cpp
+++ hw2/HW2-1.cpp
@@ -5,6 +5,21 @@ int n;
 int input_map[1000][1000];
 int value_map[1000][1000];
 
+int calculate_value(int k) {
+	memset(value_map, 0, sizeof(value_map));
+	value_map[0][0] = 1;
+	for (int i = 0; i < n; i ++) {
+		for (int j = 0; j < n; j ++) {
+			if (!i && !j) continue;
+			if (i && abs(input_map[i][j] - input_map[i - 1][j]) <= k)
+				value_map[i][j] |= value_map[i - 1][j];
+			if (j && abs(input_map[i][j] - G[i][j - 1]) <= k)
+				value_map[i][j] |= value_map[i][j - 1];
+		}
+	}
+	return dp[n - 1][n - 1];
+}
+
 int main() {
 	cin >> n;
 	for (int i = 0; i < n; i ++){
