1ca403287d18bd9c1833b0183153070189b74f8e
Ziv <zhongturtle@gmail.com>
2018-11-24 21:44:44 +0800

    Update HW2-1.cpp
    complete hw2-1

--- hw2/HW2-1.cpp
+++ hw2/HW2-1.cpp
@@ -1,35 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int input_map[1000][1000];
-int value_map[1000][1000];
 
-int calculate_value(int k) {
-	memset(value_map, 0, sizeof(value_map));
-	value_map[0][0] = 1;
+int matrix[1000][1000];
+int value[1000][1000];
+
+
+int cal_value(int k ,int n) {
+    int diff = 0;
+	memset(value, 0, sizeof(value));
+	value[0][0] = 1;
 	for (int i = 0; i < n; i ++) {
 		for (int j = 0; j < n; j ++) {
-			if (!i && !j) continue;
-			if (i && abs(input_map[i][j] - input_map[i - 1][j]) <= k)
-				value_map[i][j] |= value_map[i - 1][j];
-			if (j && abs(input_map[i][j] - G[i][j - 1]) <= k)
-				value_map[i][j] |= value_map[i][j - 1];
+			if (i){
+                diff = abs(matrix[i][j] - matrix[i - 1][j]);
+                if(diff <= k){
+                    value[i][j] |= value[i - 1][j];
+                }
+		    }
+			if (j){
+			    diff = abs(matrix[i][j] - matrix[i][j - 1]);
+                if(diff <= k){
+                    value[i][j] |= value[i][j - 1];
+                }
+			}
 		}
 	}
-	return dp[n - 1][n - 1];
+	return value[n - 1][n - 1];
 }
 
 int main() {
+    int n;
 	cin >> n;
 	for (int i = 0; i < n; i ++){
-		for (int j = 0; j < n; j ++){
-			cin >> input_map[i][j];
-    }
-  }
-
-	
-
-	cout << R << '\n';
+        for (int j = 0; j < n; j ++){
+            cin >> matrix[i][j];
+        }
+	}
+	int left = -1, right = 1e8, med;
+	while (right - left > 1) {
+		med = (left + right) / 2;
+		if (cal_value(med , n) ){
+            right = med;
+		}
+		else{
+            left = med;
+		}
+	}
+	cout << right << '\n';
 }
-
